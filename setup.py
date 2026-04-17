d883d1c4827a8a5a87d607a018e47778e2d49405
Anik <anikgupta132@gmail.com>
2026-04-18 01:46:38 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anik Gupta",
+    author_email="anikgupta132@gmail.com",
+    packages=find_packages()
+)
